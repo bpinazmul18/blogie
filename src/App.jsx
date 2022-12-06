d5365930ca2ababe99b7725a76ca5a50f14128d4
Nazmul Haque <bpinazmul18@gmail.com>
2022-12-06 20:21:36 +0600

    Add postlist component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Router from './Router'
 const App = () => {
   return (
-    <div className="app-main">
+    <div className="app-main ui container">
       <Router />
     </div>
   )
