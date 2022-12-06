b7a4f9b83b88bbcc2acc741073347b1a2c9f540f
Nazmul Haque <bpinazmul18@gmail.com>
2022-12-06 21:34:07 +0600

    Refactoring blogs to posts.

---
+++ src/store/posts.js
@@ -0,0 +1,17 @@
+// ActionTypes
+
+// Actions
+
+// Reducer
+
+const initalState = {
+  list: [],
+  selectPosts: null,
+  lastFetch: null,
+}
+
+const postsReducer = (posts = initalState, action) => {
+  return posts
+}
+
+export default postsReducer
