eb586de90e0f6de4a90307e1079ec38182e73d2f
Nazmul Haque <bpinazmul18@gmail.com>
2022-12-06 19:56:31 +0600

    Setup store.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
+import configureStore from './store/configureStore'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-  //   <Provider>
-  <App />
-  //   </Provider>
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>
 )
